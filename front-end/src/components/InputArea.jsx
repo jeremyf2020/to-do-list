9465721d73c4263fc398d8e18abfe40886304f99
jeremyf2020 <jeremy2020fan@gmail.com>
2024-07-30 15:56:44 +0100

    finish option menu

--- front-end/src/components/InputArea.jsx
+++ front-end/src/components/InputArea.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react'
+import OptionMenu from './OptionMenu';
 
 
 
@@ -7,9 +8,7 @@ export default function InputArea({ dispatch }) {
     console.log('rendering InputArea')
     /* using state not ref because [TODO: add task suggestion later] */
     const [taskInput, setTaskInput] = useState("");
-    const [focusing, setFocusing] = useState(false);
-    const catergyInputRef = useRef(null);
-    const [customChecked, setCustomChecked] = useState(false)
+    const [showOptionMenu, setShowOptionMenu] = useState(false);
 
     function addTask(text) {
         dispatch({ type: 'add_task', text: text })
@@ -26,12 +25,13 @@ export default function InputArea({ dispatch }) {
         const formJson = Object.fromEntries(formData.entries());
 
         // Add Task
+        console.log(formJson.category)
 
         console.log(formJson)
 
         // Clear the form
         setTaskInput('');
-        setFocusing(false);
+        setShowOptionMenu(false);
     }
 
 
@@ -39,30 +39,11 @@ export default function InputArea({ dispatch }) {
         <form method="post" onSubmit={handleSubmit}>
             <label>
                 Add to-do task:
-                <input onFocus={() => setFocusing(true)} name="newtask" value={taskInput} onChange={e => setTaskInput(e.target.value)} />
+                <input onFocus={() => setShowOptionMenu(true)} name="newtask" value={taskInput} onChange={e => setTaskInput(e.target.value)} />
             </label>
             <button>+</button>
 
-            {focusing && <>
-                <div>
-                    Categories:
-                    <label><input type="radio" name="catergy" value="default" defaultChecked={true} />Default</label>
-                    {" "}
-                    <label><input type="radio" name="catergy" value="option2" /> Option 2</label>
-                    <label><input type="radio" name="catergy" value="option3" /> Option 3</label>
-                    <br />
-                    <label><input type="radio" name="catergy"
-                        checked={customChecked} onChange={() => { setCustomChecked(true) }}
-                        value={catergyInputRef.current !== null && catergyInputRef.current.value} />Custom:
-                        <input type="text" ref={catergyInputRef} onFocus={() => { setCustomChecked(true) }} />
-                    </label>
-                </div>
-                <div>
-                    <label>Start Time<input type="datetime-local" name="startTime" /> </label>
-                    <br />
-                    <label>End Time<input type="datetime-local" name="endTime" /> </label>
-                </div>
-            </>}
+            {showOptionMenu && <OptionMenu />}
         </form>
     )
 }
---
+++ front-end/src/components/OptionMenu.jsx
@@ -0,0 +1,51 @@
+import React, { forwardRef, useEffect, useRef, useState } from 'react'
+
+const samplecategory = ['None', 'C.S.', 'English', 'Home']
+
+export default function OptionMenu() {
+    const [categoryBtnSelection, setCategoryBtnSelection] = useState("None");
+
+    const [disableCustomInput, setDisableCustomInput] = useState(true);
+    const categoryInputRef = useRef("");
+
+    useEffect(() => {
+        if (!disableCustomInput && categoryBtnSelection === "Custom") {
+            categoryInputRef.current.focus();
+        }
+    }, [disableCustomInput, categoryBtnSelection]);
+
+
+    function handleCustomCatergoy(target) {
+        setDisableCustomInput(target === "None")
+        setCategoryBtnSelection(target)
+    }
+
+    return (
+        <>
+            <div >
+                Categories:
+                <div className={`btn ${categoryBtnSelection === "None" && "active"}`}
+                    onClick={() => handleCustomCatergoy("None")}>None</div>
+                <div className={`btn ${categoryBtnSelection === "Custom" && "active"}`}
+                    onClick={() => handleCustomCatergoy("Custom")}>Custom</div>
+                <div >
+                    <CustomInput disableCustomInput={disableCustomInput} ref={categoryInputRef} />
+                </div>
+            </div>
+            <div>
+                <label>Start Time<input type="datetime-local" name="startTime" /> </label>
+                <br />
+                <label>End Time<input type="datetime-local" name="endTime" /> </label>
+            </div>
+        </>
+    )
+}
+
+
+
+const CustomInput = forwardRef(({ disableCustomInput }, ref) => {
+    return (
+        <input name="category" disabled={disableCustomInput} ref={ref}></input>
+    );
+});
+
