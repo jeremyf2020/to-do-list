ad88daf8001223275adfdb8e116cbd95d00c9859
jeremyf2020 <jeremy2020fan@gmail.com>
2024-08-08 14:47:59 +0100

    edit category funC in category bar

--- front-end/src/components/CategoryEditField.jsx
+++ front-end/src/components/CategoryEditField.jsx
@@ -1,15 +1,24 @@
 import { Pencil, X, Save } from 'lucide-react';
-import React from 'react'
+import React, { useContext, useRef } from 'react'
+import { CategoriesContext } from "../App";
 
 export default function CategoryEditField({ selectedCategory, setSelectedCategory, value, objkey, editingCategory, handleDelete }) {
+    const fieldRef = useRef(value)
+    const { categories, setCategories } = useContext(CategoriesContext);
 
-
+    function handleSave(objkey) {
+        setCategories(prevCategories => ({
+            ...prevCategories,
+            [objkey]: fieldRef.current.value
+        }));
+    }
 
     return (
         <div className='relative'>
             <input
                 className={`mx-1 rounded-xl cursor-pointer text-center outline-none border-none 
                             ${selectedCategory === value ? "bg-amber-600 text-lime-50" : "bg-white"}`}
+                ref={fieldRef}
                 defaultValue={value}
                 readOnly={value === "All" ? true : !editingCategory}
                 size={value.length}
