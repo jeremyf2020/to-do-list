1c571fef1c1be867d8793c24d2cba2ce3f4f504b
jeremyf2020 <jeremy2020fan@gmail.com>
2024-08-23 14:06:47 +0100

    Revert "add: add btn on beside category"
    This reverts commit a8c477d7984955e50bea44d7c7da3cef4eac5510.

--- front-end/src/components/CategoryList.jsx
+++ front-end/src/components/CategoryList.jsx
@@ -1,20 +1,15 @@
 import React, { useState } from 'react'
 import TaskLine from './TaskLine'
-import { ChevronDown, ChevronRight, CirclePlus } from 'lucide-react'
+import { ChevronDown, ChevronRight } from 'lucide-react'
 
 export default function CategoryList({ title, tasks, categoryId, tasksDispatch }) {
     const [showList, setShowList] = useState(true)
-
-    function handleAdd(categoryId) {
-        tasksDispatch({ type: 'add_task', object: { id: crypto.randomUUID(), category_id: categoryId } })
-    }
 
     return (
         <>
             <div className='flex'>
                 <button onClick={() => { setShowList(!showList) }}>{showList ? <ChevronDown /> : <ChevronRight />}</button>
                 <h2 className='text-xl font-bold ' >{title !== "All" ? title : "Default"}</h2>
-                <button className='size-6 text-base text-zinc-400' onClick={() => handleAdd(categoryId)}><CirclePlus size={16} strokeWidth={2} /></button>
             </div>
             {showList &&
                 <ul className='ml-6'>
